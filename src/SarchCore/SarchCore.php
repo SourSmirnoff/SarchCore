d184f086fa71f9bdec2ead692517fd1721656fe7
SourSmirnoff <davenadams2816@gmail.com>
2017-09-12 22:18:34 -0700

    Prepare for Bounty/Spawner(s)/Shop
    Prepare Main File for Adding SilkSpawners, SpawnerShop, And Bounty System. Prepare for resources Files. Will Clean up Later.

--- src/SarchCore/SarchCore.php
+++ src/SarchCore/SarchCore.php
@@ -10,22 +10,41 @@ use SarchCore\Envoys\EnvoyManager;
 use SarchCore\Security\SecurityManager;
 use SarchCore\Staff\StaffManager;
 use SarchCore\Tasks\MobClearTask;
+use SarchCore\Bounties\BountyManager;
+use SarchCore\Spawners\SilkListener;
+use SarchCore\Spawners\ShopListener;
 
 class SarchCore extends PluginBase {
 
-	protected $weaponsmanager, $staffmanager, $securitymanager, $envoymanager;
+	protected $shoplistener, $bountymanager, $weaponsmanager, $staffmanager, $securitymanager, $envoymanager, $cheatmanager, $silklistener;
 
+	protected static $allowed, $spawners = [];
+	
 	public function onEnable() {
-		@mkdir($this->getDataFolder());
-		@mkdir($this->getDataFolder() . "/factions");
+		/*@mkdir($this->getDataFolder());     Not in use Currently
+		@mkdir($this->getDataFolder() . "/factions");*/
 		$this->getServer()->getPluginManager()->registerEvents(($this->staffmanager = new StaffManager($this)), $this);
 		$this->getServer()->getPluginManager()->registerEvents(($this->cheatmanager = new CheatManager($this)), $this);
 		$this->getServer()->getPluginManager()->registerEvents(($this->weaponsmanager = new CustomWeaponsManager($this)), $this);
 		$this->getServer()->getPluginManager()->registerEvents(($this->securitymanager = new SecurityManager($this)), $this);
 		$this->getServer()->getPluginManager()->registerEvents(($this->envoymanager = new EnvoyManager($this)), $this);
-		// Classes Removed $this->getServer()->getCommandMap()->register("class", new ClassCommand($this));
 		$this->getServer()->getScheduler()->scheduleRepeatingTask(new MobClearTask($this), 20 * (60 * 5));
 		$this->getServer()->getScheduler()->scheduleRepeatingTask(new EnvoyManager($this), 20 * (60 * 5));
+		$this->getServer()->getCommandMap()->register("bounty", new BountyCommand($this));
+                $this->getServer()->getCommandMap()->register("spawner", new SpawnerShopCommand($this));
+		
+		if (!file_exists($this->getDataFolder() . 'config.yml')) {
+			@mkdir($this->getDataFolder());
+			file_put_contents($this->getDataFolder() . 'config.yml', $this->getResource('config.yml'));
+		}
+
+		foreach ($this->getConfig()->get('spawners') as $id => $name) {
+			self::$spawners[$id] = $name;
+		}
+
+		foreach ($this->getConfig()->get('allowed') as $items) {
+			self::$allowed[$items] = true;
+		}
 	}
 
 	public function getEnvoyManager() {
@@ -34,5 +53,23 @@ class SarchCore extends PluginBase {
 
 	public function getSecurityManager() {
 		return $this->securitymanager;
+	}
+	public function getShopListener() {
+		return $this->shoplistener;
+	}
+        public function getSilkListener() {
+		return $this->silklistener;
+	}
+        
+        private static function parseColors(string $string) {
+		return str_replace('&', TextFormat::ESCAPE, $string);
+	}
+
+	public static function retrieveName(int $entityId) {
+		return isset(self::$spawners[$entityId]) ? TextFormat::RESET.self::parseColors(self::$spawners[$entityId]).TextFormat::RESET : null;
+	}
+
+	public static function isAllowed($itemId) {
+		return isset(self::$allowed[$itemId]) ? true : false;
 	}
 }
